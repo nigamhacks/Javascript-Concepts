717bbdb583f6dcc8d036e9230d5c80c6bc1d3986
nigamhacks <sachinnigam9@live.com>
2025-07-11 00:54:53 +0530

    first commit

---
+++ inbuiltObjects.js
@@ -0,0 +1,14 @@
+console.log("Hello Objects");
+
+console.log(Math.PI);
+console.log(Math.max(40,38,19,90));
+
+console.log(Math.min(30,39,219,10));
+
+console.log(Math.round(1.9));
+
+console.log(Math.ceil(1.5));
+console.log(Math.floor(1.5));
+
+console.log(Math.abs(-7));
+
