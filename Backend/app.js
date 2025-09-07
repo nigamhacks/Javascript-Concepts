50ca9b637a8a51ead4b76cce639ba08bc35ba16e
nigamhacks <sachinnigam9@live.com>
2025-09-07 21:04:54 +0530

    second commit

--- Backend/app.js
+++ Backend/app.js
@@ -1,15 +1,15 @@
-const { readFile, writeFile } = require("fs");
-const util = require('util');
-const readFilePromise= util.promisify(readFile);
+const { readFile, writeFile } = require("fs").promises;
+// const util = require('util');
+// const readFilePromise= util.promisify(readFile);
 
-const writeFilePromise= util.promisify(writeFile);
+// const writeFilePromise= util.promisify(writeFile);
 
 const start = async () => {
   try {
-    const first = await readFilePromise("./first.txt", 'utf8');
-    const second = await readFilePromise("./second.txt", 'utf8');
+    const first = await readFile("./first.txt", 'utf8');
+    const second = await readFile("./second.txt", 'utf8');
     //first two promises to read file 
-    await writeFilePromise('./data.txt', `This is awesome : ${first} ${second}`);
+    await writeFile('./data.txt', `This is awesome : ${first} ${second}`);
     //this promise is used to write the data.txt file with contents of first and second text file
     console.log(first, second);
 
